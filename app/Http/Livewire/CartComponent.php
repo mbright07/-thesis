f82defa98c48c54644aced048d0ce79a6a464d88
Hoàng Thị Thu Trang <61085730+tranghoangthu812102@users.noreply.github.com>
2022-03-03 08:58:15 +0700

    Delete Job Bookmark

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -3,9 +3,22 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Cart;
 
 class CartComponent extends Component
 {
+    public function destroy($rowId)
+    {
+        Cart::remove($rowId);
+        session()->flash('success_message', 'Bookmark has been removed!');
+    }
+
+    public function destroyAll()
+    {
+        Cart::destroy();
+        session()->flash('success_message', 'All Bookmark has been removed!');
+    }
+
     public function render()
     {
         return view('livewire.cart-component')->layout("layouts.base");
