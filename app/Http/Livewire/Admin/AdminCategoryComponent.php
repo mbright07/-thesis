b2a97da04bb7a3575591ffdec937fff27efd5086
Hoàng Thị Thu Trang <61085730+tranghoangthu812102@users.noreply.github.com>
2022-03-04 10:49:50 +0700

    Admin Delete Locations

--- app/Http/Livewire/Admin/AdminCategoryComponent.php
+++ app/Http/Livewire/Admin/AdminCategoryComponent.php
@@ -8,6 +8,15 @@ use Livewire\WithPagination;
 
 class AdminCategoryComponent extends Component
 {
+    use WithPagination;
+
+    public function deleteCategory($id)
+    {
+        $category = Category::find($id);
+        $category->delete();
+        session()->flash('message', 'Location has been deleted successfully!');
+    }
+
     public function render()
     {
         $categories = Category::paginate(5);
