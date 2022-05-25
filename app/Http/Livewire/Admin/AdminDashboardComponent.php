d8a47b5464887fc1e849550186582926e00a488a
Hoàng Thị Thu Trang <61085730+tranghoangthu812102@users.noreply.github.com>
2022-05-25 12:40:33 +0700

    Display Data On Admin Dashboard

--- app/Http/Livewire/Admin/AdminDashboardComponent.php
+++ app/Http/Livewire/Admin/AdminDashboardComponent.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Recruitment;
+use Carbon\Carbon;
 use Livewire\Component;
 
 class AdminDashboardComponent extends Component
 {
     public function render()
     {
-        return view('livewire.admin.admin-dashboard-component')->layout("layouts.base");
+        $recruitments = Recruitment::orderBy('created_at', 'DESC')->get()->take(10);
+        $totalRecruitments = Recruitment::where('status', 'pending')->count();
+        $totalRecruitments_processing = Recruitment::where('status', 'processing')->count();
+        $totalRecruitments_canceled = Recruitment::where('status', 'canceled')->count();
+        $totalRecruitments_today = Recruitment::where('status', 'pending')->whereDate('created_at', Carbon::today())->count();
+        return view('livewire.admin.admin-dashboard-component', [
+            'recruitments' => $recruitments,
+            'totalRecruitments' => $totalRecruitments,
+            'totalRecruitments_processing' => $totalRecruitments_processing,
+            'totalRecruitments_canceled' => $totalRecruitments_canceled,
+            'totalRecruitments_today' => $totalRecruitments_today
+        ])->layout("layouts.base");
     }
 }
