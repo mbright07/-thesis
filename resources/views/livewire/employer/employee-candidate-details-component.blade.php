f91bb5f190a08999874e4e23446722e49a8eeb31
Hoàng Thị Thu Trang <61085730+tranghoangthu812102@users.noreply.github.com>
2022-07-11 12:04:40 +0700

    candidate-details-comment

--- resources/views/livewire/employer/employee-candidate-details-component.blade.php
+++ resources/views/livewire/employer/employee-candidate-details-component.blade.php
@@ -1,10 +1,261 @@
 <main id="main" class="main-site">
+    <div class="container">
+        <div class="wrap-breadcrumb">
+            <ul>
+                <li class="item-link"><a href="{{ route('employer.home') }}" class="link">{{ __('detail.home') }}</a>
+                </li>
+                <li class="item-link"><span>{{ __('detail.detail') }}</span></li>
+            </ul>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 col-md-8 col-sm-8 col-xs-12 main-content-area">
+                <div class="wrap-product-detail">
+                    <div class="container-details">
+                        <div class="detail-media" style="width: 25%; float:left">
+                            @if ($user->profile && $user->profile->image)
+                                <img src="{{ asset('assets/images/profile') }}/{{ $user->profile->image }}"
+                                    alt="{{ $user->name }}" height="300" width="300" />
+                            @else
+                                <img src="http://ssl.gstatic.com/accounts/ui/avatar_2x.png" width="300"
+                                    height="300" alt="{{ $user->name }}" />
+                            @endif
+                        </div>
+
+                        <div class="detail-info" style="width: 40%; margin-top: -40px; ">
+                            <div class="product-rating">
+                                <style>
+                                    .color-gray {
+                                        color: #e6e6e6 !important;
+                                    }
+                                </style>
+                                @for ($i = 1; $i <= 5; $i++)
+                                    @if ($i <= $user->rating_avg)
+                                        <i class="fa fa-star" aria-hidden="true"></i>
+                                    @else
+                                        <i class="fa fa-star color-gray" aria-hidden="true"></i>
+                                    @endif
+                                @endfor
+                                <a href="#" class="count-review">({{ $user->review_cnt }}
+                                    {{ __('detail.review') }})</a>
+                            </div>
+
+                            <div class="short-desc">
+                                <h2 class="product-name">{{ $user->name }}</h2>
+                                <h5><i class="fa fa-eye" aria-hidden="true"></i>
+                                    {{ $user->totalviews }}</h5>
+                                <table class="table table-bordered">
+                                    <thead>
+                                        <th>{{ __('employee/home.expected_location') }}</th>
+                                        <th>{{ __('detail.salary') }}</th>
+                                    </thead>
+                                    <tbody>
+                                        @if ($user->workPreference)
+                                            @foreach ($user->workPreference as $item)
+                                                <tr>
+                                                    <td>
+                                                        {{ $item->expected_location_name }}
+                                                    </td>
+                                                    <td>
+                                                        {{ $item->expected_salary }}
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        @endif
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="stock-info in-stock">
+                                <p class="availability">
+                                    <i class="fa fa-check" aria-hidden="true"></i>:
+                                    <b>{{ $available ? 'Available' : 'Unavailable' }}</b>
+                                </p>
+                            </div>
+                            <div class="wrap-butons" style="margin-top: -30px;">
+                                <a href="#" class="btn add-to-cart"
+                                    wire:click.prevent="company({{ $user->id }},'{{ $user->name }}',{{ json_encode($user->workPreference) }})">{{ __('detail.bookmark') }}</a>
+                            </div>
+                        </div>
+
+                        <div class="widget widget-our-services " style="width: 30%; float: right;">
+                            <div class="widget-content">
+                                <ul class="our-services">
+                                    <li class="service">
+                                        <a class="link-to-service" href="#">
+                                            <i class="fa fa-truck" aria-hidden="true"></i>
+                                            <div class="right-content">
+                                                <b class="title">{{ __('detail.career') }}</b>
+                                                <span class="subtitle"></span>
+                                                <p class="desc">Lorem Ipsum is simply dummy text of the
+                                                    printing...
+                                                </p>
+                                            </div>
+                                        </a>
+                                    </li>
+
+                                    <li class="service">
+                                        <a class="link-to-service" href="#">
+                                            <i class="fa fa-gift" aria-hidden="true"></i>
+                                            <div class="right-content">
+                                                <b class="title">{{ __('detail.skill') }}</b>
+                                                <span class="subtitle"></span>
+                                                <p class="desc">Lorem Ipsum is simply dummy text of the
+                                                    printing...
+                                                </p>
+                                            </div>
+                                        </a>
+                                    </li>
+
+                                    <li class="service">
+                                        <a class="link-to-service" href="#">
+                                            <i class="fa fa-reply" aria-hidden="true"></i>
+                                            <div class="right-content">
+                                                <b class="title">{{ __('detail.area') }}</b>
+                                                <span class="subtitle"></span>
+                                                <p class="desc">Lorem Ipsum is simply dummy text of the
+                                                    printing...
+                                                </p>
+                                            </div>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="advance-info">
+                        <div class="tab-control normal">
+                            <a href="#description" class="tab-control-item active">{{ __('detail.cv') }}</a>
+                            <a href="#add_infomation" class="tab-control-item">{{ __('detail.can_info') }}</a>
+                            <a href="#review" class="tab-control-item">{{ __('detail.review') }}</a>
+                        </div>
+                        <div class="tab-contents">
+                            <div class="tab-content-item active" id="description">
+                                @include('livewire.employer.employee-candidate-resume-component')
+                            </div>
+
+                            <div class="tab-content-item " id="add_infomation">
+                                <div class="col-sm-9">
+                                    <div class="tab-content">
+                                        <ul class="list-in-text">
+                                            <li><i class="fa fa-user" aria-hidden="true"></i> {{ $user->name }}
+                                            </li>
+                                            <li><i class="fa fa-envelope" aria-hidden="true"></i>
+                                                {{ $user->email }}
+                                            </li>
+                                            <li><i class="fa fa-phone" aria-hidden="true"></i>
+                                                {{ $user->profile ? $user->profile->mobile : '' }}</li>
+                                            <li><i class="fa fa-map-marker" aria-hidden="true"></i>
+                                                {{ $user->profile ? $user->profile->address : '' }}</li>
+                                            <li><i class="fa fa-info-circle" aria-hidden="true"></i>
+                                                <cite>{{ $user->profile ? $user->profile->intro : '' }}</cite>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="tab-content-item " id="review">
+
+                                <div class="wrap-review-form">
+                                    <style>
+                                        .width-0-percent {
+                                            width: 0%;
+                                        }
+
+                                        .width-20-percent {
+                                            width: 20%;
+                                        }
+
+                                        .width-40-percent {
+                                            width: 40%;
+                                        }
+
+                                        .width-60-percent {
+                                            width: 60%;
+                                        }
+
+                                        .width-80-percent {
+                                            width: 80%;
+                                        }
+
+                                        .width-100-percent {
+                                            width: 100%;
+                                        }
+                                    </style>
+                                    <div id="comments">
+                                        @if (Session::has('message'))
+                                            <div class="alert alert-success" role="alert">
+                                                {{ Session::get('message') }}</div>
+                                        @endif
+                                        <h2 class="woocommerce-Reviews-title">
+                                            {{ $user->review_cnt }}
+                                            {{ __('detail.review_for') }} <span>{{ $user->name }}</span></h2>
+                                        <ol class="commentlist">
+                                            @foreach ($user->recruitments as $recruitment)
+                                                @foreach ($recruitment->recruitmentJob as $recruitmentJob)
+                                                    @foreach ($recruitmentJob->reviewCandidates as $review)
+                                                        <li class="comment byuser comment-author-admin bypostauthor even thread-even depth-1"
+                                                            id="li-comment-20">
+                                                            <div id="comment-20" class="comment_container">
+                                                                <img alt=""
+                                                                    src="{{ asset('assets/images/author-avata.jpg') }}"
+                                                                    height="80" width="80">
+                                                                <div class="comment-text">
+                                                                    <div class="star-rating">
+                                                                        <span
+                                                                            class="width-{{ $review->rating * 20 }}-percent">{{ __('detail.rate') }}
+                                                                            <strong
+                                                                                class="rating">{{ $review->rating }}</strong>
+                                                                            {{ __('detail.out_of_5') }} </span>
+                                                                    </div>
+                                                                    <p class="meta">
+                                                                        <strong
+                                                                            class="woocommerce-review__author">{{ $recruitmentJob->job->user->name }}</strong>
+                                                                        <span class="woocommerce-review__dash">–</span>
+                                                                        <time
+                                                                            class="woocommerce-review__published-date"
+                                                                            datetime="2008-02-14 20:00">{{ Carbon\Carbon::parse($review->created_at)->format('d F Y g:i A') }}</time>
+                                                                    </p>
+                                                                    <div class="description">
+                                                                        <p>{{ $review->comment }}</p>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            @if ($recruitmentJob->job->user_id == Auth::user()->id)
+                                                                <div class="panel-body-right" style="float: right">
+                                                                    <i class="fa fa-trash" aria-hidden="true"
+                                                                        onclick="confirm('Are you sure?') || event.stopImmediatePropagation()"
+                                                                        wire:click.prevent="deleteReview({{ $review->id }})"
+                                                                        style="color: rgb(248, 66, 66); font-size: 18px;"></i>
+                                                                </div>
+                                                            @endif
+                                                        </li>
+                                                    @endforeach
+                                                @endforeach
+                                            @endforeach
+                                        </ol>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!--end row-->
+
+    </div>
+    <!--end container-->
+
+</main>
+{{-- <main id="main" class="main-site">
 
     <div class="container">
 
         <div class="wrap-breadcrumb">
             <ul>
-                {{--                <li class="item-link"><a href="/" class="link">{{ __('detail.home') }}</a></li>--}}
                 <li class="item-link"><span>{{ __('detail.detail') }}</span></li>
             </ul>
         </div>
@@ -12,7 +263,7 @@
             <div class="col-lg-12 col-md-8 col-sm-8 col-xs-12 main-content-area">
                 <div class="wrap-product-detail">
                     <div class="detail-media">
-                        @if($user->profile && $user->profile->image)
+                        @if ($user->profile && $user->profile->image)
                             <img src="{{ asset('assets/images/profile') }}/{{ $user->profile->image }}" alt="{{ $user->name }}"
                                  height="300" width="300" />
                         @else
@@ -47,8 +298,8 @@
                                 <th>{{ __('detail.salary') }}</th>
                                 </thead>
                                 <tbody>
-                                @if($user->workPreference)
-                                    @foreach($user->workPreference as $item)
+                                @if ($user->workPreference)
+                                    @foreach ($user->workPreference as $item)
                                         <tr>
                                             <td>
                                                 {{ $item->expected_location_name }}
@@ -86,7 +337,7 @@
                             <div class="tab-content-item " id="add_infomation">
                                 <div class="col-sm-3">
                                     <div class="text-center">
-                                        @if($user->profile && $user->profile->image)
+                                        @if ($user->profile && $user->profile->image)
                                             <img src="{{ asset('/assets/images/profile') }}/{{ $user->profile->image }}"
                                                  class="avatar img-circle img-thumbnail" alt="avatar">
                                         @endif
@@ -249,7 +500,7 @@
                                                                     </div>
                                                                 </div>
                                                             </div>
-                                                            @if($recruitmentJob->job->user_id == Auth::user()->id)
+                                                            @if ($recruitmentJob->job->user_id == Auth::user()->id)
                                                                 <div class="panel-body-right" style="float: right">
                                                                     <i class="fa fa-trash" aria-hidden="true"
                                                                        onclick="confirm('Are you sure?') || event.stopImmediatePropagation()"
@@ -277,4 +528,4 @@
     </div>
     <!--end container-->
 
-</main>
+</main> --}}
